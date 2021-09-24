432265f111f6b38bc3a717761959040ba0e76104
Rafael Farias <rafael.farias@webjump.com.br>
2021-09-24 11:31:53 -0300

    Adição de retorno de erro 404 caso a URL seja inválida.

--- Public/index.php
+++ Public/index.php
@@ -6,8 +6,12 @@ require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../Config/routes.php';
 require_once __DIR__ . '/../Config/helpers.php';
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+
 try {
     SimpleRouter::start();
 } catch (Exception $exception) {
-    var_export($exception);
+    response()->httpCode(404);
+    response()->json(['message' => 'Page not found']);
 }
